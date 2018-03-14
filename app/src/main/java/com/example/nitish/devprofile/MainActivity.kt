d01d2178d4a4378023b4601bc25f41195a7b1a25
Nitish Rana <nitish@maropost.com>
2018-03-14 14:59:09 +0530

    Added portrait layout for tablet

--- app/src/main/java/com/example/nitish/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/nitish/devprofile/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.nitish.devprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        var rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.isCircular = true
+        logo.setImageDrawable(rounded)
     }
 }
